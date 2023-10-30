3007026164404214099b58fb8dbc40690bc7a11e
temitopefa <lasbohoo@gmail.com>
2023-10-30 20:04:25 +0100

    fix on resort

--- src/components/Locations.tsx
+++ src/components/Locations.tsx
@@ -7,17 +7,17 @@ const Locations:React.FC = () => {
       <div className='h-[64rem] w-[30rem] relative '>
         <img src={resort} className='object-cover h-full w-full'/>
         <div className="absolute inset-0 bg-black opacity-30"></div>
-        <p className='transform -rotate-90 origin-top absolute text-white text-6xl mt-[-20rem] ml-[6rem] font-semibold tracking-tight leading-normal' style={{ width:"max-content" }}>Mountain Resort</p>
+        <p className='transform -rotate-90 origin-top absolute text-white text-6xl mt-[-20rem] ml-[5rem] font-semibold tracking-tight leading-normal' style={{ width:"max-content" }}>Mountain Resort</p>
       </div>
       <div className='h-[64rem] w-[30rem] relative'>
         <img src={lake} className='object-cover h-full w-full' />
         <div className="absolute inset-0 bg-black opacity-30"></div>
-        <p className='transform -rotate-90 origin-top absolute text-white text-6xl mt-[-18rem] ml-[9rem] font-semibold tracking-tight leading-normal' style={{ width:"max-content" }}>Fuji Mountain</p>
+        <p className='transform -rotate-90 origin-top absolute text-white text-6xl mt-[-18rem] ml-[7rem] font-semibold tracking-tight leading-normal' style={{ width:"max-content" }}>Fuji Mountain</p>
       </div>
       <div className='h-[64rem] w-[30rem] relative'>
         <img src={fuji} className='object-cover h-full w-full' />
         <div className="absolute inset-0 bg-black opacity-30"></div>
-        <p className='transform -rotate-90 origin-top absolute text-white text-6xl mt-[-22rem] ml-[3rem] font-semibold tracking-tight leading-normal' style={{ width:"max-content" }}>Freezing WinterLake</p>
+        <p className='transform -rotate-90 origin-top absolute text-white text-6xl mt-[-22rem] ml-[2rem] font-semibold tracking-tight leading-normal' style={{ width:"max-content" }}>Freezing WinterLake</p>
       </div>
     </div>
   )
